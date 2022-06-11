61c116bc43c9c7acc26cb109e6977fe75082355e
Adarsh Gupta <hi@adarshgupta.dev>
2022-06-11 22:49:06 +0530

    completed chapter 4

---
+++ Book 1 Django for Beginners/Chapter 04: Message Board App/posts/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import ListView
+
+from .models import Post
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "posts/home.html"
+    context_object_name = 'all_posts_list'
